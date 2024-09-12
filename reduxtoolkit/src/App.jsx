53e2ac9cc3a826970cbc720cccbe39241f008e0f
MaryamRafiqueBhatti <2021ee290@student.uet.edu.pk>
2024-09-12 22:09:38 +0500

    some more projects

--- reduxtoolkit/src/App.jsx
+++ reduxtoolkit/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from "/src/components/Todos";
+import AddTodo from "/src/components/AddTodo";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <h1>studying redux toolkit</h1>
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
   )
 }
 
--- reduxtoolkit/src/app/store.js
+++ reduxtoolkit/src/app/store.js
@@ -1,7 +1,6 @@
-import {configureStore} from '@reduxjs/toolkit'
-import todoReducer from '../features/todo/todoSlice'
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
 
-
-export const Store = configureStore({
+export const store = configureStore({
     reducer: todoReducer
 })
